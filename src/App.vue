5557d8b742e33107883b1f761edd974966cc28b8
V-Hugo_505 <victorhugomoura055@gmail.com>
2026-06-09 17:42:30 +0000

    Colocando barra de progresso

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@ import { ref } from 'vue'
 const count = ref(0)
 import Calendario from './components/Calendario.vue';
 import Footer from './components/Footer.vue';
+import Progresso from './components/Progresso.vue';
 
 </script>
 
 <template>
+  <Progresso :steps="4" :current-step="1" />
   <Calendario />
   <Footer />
+  
 </template>
 
 <style scoped>
---
+++ src/components/Progresso.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full flex">
+    <div
+      v-for="(step, index) in steps"
+      :key="index"
+      class="h-1.5 flex-1 transition-colors duration-300"
+      :class="index < currentStep ? 'bg-[#2D6E6A]' : 'bg-[#B5A89A]'"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  steps: {
+    type: Number,
+    default: 3
+  },
+  currentStep: {
+    type: Number,
+    default: 1
+  }
+})
+</script>
