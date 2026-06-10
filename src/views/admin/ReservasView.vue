0756f6988872ff8093da8d98bd926537c2ce7abd
Ricki-h <rick1n.htz@gmail.com>
2026-06-10 01:12:33 -0300

    começando reservas

--- src/views/admin/ReservasView.vue
+++ src/views/admin/ReservasView.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from 'vue'
 
 import api from '../../services/api'
 import ReservasAdminGrid from '../../components/ReservasAdminGrid.vue'
+import Input from '../../components/Input.vue'
 
 const reservas = ref([])
 const loading = ref(true)
@@ -22,6 +23,27 @@ onMounted(carregarReservas)
 </script>
 
 <template>
+  <Header variant="admin"></Header>
+  
+  <main class="pt-30 md:pt-35 justify-center flex px-6 sm:px-12">
+    <div class="container-app">
+      <div class="flex flex-col gap-6">
+        <div>
+          <div>
+            <h1 class="text-5xl sm:text-6xl text-gold-500">Reservas</h1>
+            <h3 class="text-base sm:text-xl">Todas as reservas do sistema</h3>
+          </div>
+          
+
+        </div>
+        
+      </div>
+
+    </div>
+    
+  </main>
+
+  <Rodape/>
   <ReservasAdminGrid
     :reservas="reservas"
     :loading="loading"
